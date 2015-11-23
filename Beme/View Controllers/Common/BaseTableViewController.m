b6af8de7b006f951b6de25b2861aba2d3b052fef
Eric Lewis <ericlewis777@gmail.com>
2015-11-22 22:29:08 -0800

    handle edge to edge cells

--- Beme/View Controllers/Common/BaseTableViewController.m
+++ Beme/View Controllers/Common/BaseTableViewController.m
@@ -39,4 +39,22 @@
     [self.tableView setTableFooterView:[UIView new]];
 }
 
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Remove seperator inset
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    // Prevent the cell from inheriting the Table View's margin settings
+    if ([cell respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [cell setPreservesSuperviewLayoutMargins:NO];
+    }
+    
+    // Explictly set your cell's layout margins
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+
 @end
