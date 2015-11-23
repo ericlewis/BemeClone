aada2c8a844a0948f547b33662f485c23d644810
Eric Lewis <ericlewis777@gmail.com>
2015-11-23 12:25:49 -0800

    capture is much improved, only stores locally for now. but timing is on point.

--- Beme/View Controllers/Video/CaptureViewController.m
+++ Beme/View Controllers/Video/CaptureViewController.m
@@ -7,13 +7,21 @@
 //
 
 #import "CaptureViewController.h"
+
+// for playing vibration
 #import <AudioToolbox/AudioToolbox.h>
+
+// for saving the data locally
 #import <MobileCoreServices/MobileCoreServices.h>
+
+// for saving remotely
 #import <Parse/Parse.h>
 
 @interface CaptureViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (nonatomic, strong) NSTimer *finishTimer;
+@property (nonatomic, strong) NSTimer *videoRecordTimer;
 @property (nonatomic, strong) BaseLabel *captureStatusLabel;
+@property (nonatomic) BOOL videoIsLongEnough;
 @end
 
 @implementation CaptureViewController
@@ -27,6 +35,9 @@
         self.cameraFlashMode = UIImagePickerControllerCameraFlashModeOff;
         self.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
         self.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
+        
+        // this is lame :(
+        //self.videoMaximumDuration = 7;
 
         // for some reason, reusing this is not a great idea?
         if (!self.captureStatusLabel) {
@@ -53,24 +64,27 @@
 
 - (void)sensorStateChange:(NSNotificationCenter *)notification
 {
+    // device is no longer held close.
     if ([[UIDevice currentDevice] proximityState] == NO){
+        
+        // stop the video capture now.
         [self stopVideoCapture];
-
-        if (self.finishTimer.isValid) {
+        [self.finishTimer invalidate];
+        [self.videoRecordTimer invalidate];
+        
+        // if the video isn't long enough, say cancelled. if it is, just blank it out.
+        if (!self.videoIsLongEnough) {
             // set the capture status to cancelled since they decided not to wait long enough
             self.captureStatusLabel.text = @"CANCELLED";
             self.captureStatusLabel.textColor = [UIColor whiteColor];
+            
+            [self performSelector:@selector(dismissVC) withObject:nil afterDelay:0.4];
         }else{
             // reset for record mode since we recycle the view
             self.captureStatusLabel.text = @"";
             self.captureStatusLabel.textColor = [UIColor redColor];
         }
         
-        // fuck the timer though
-        [self.finishTimer invalidate];
-    }else{
-        self.captureStatusLabel.textColor = [UIColor redColor];
-        self.captureStatusLabel.text = @"RECORDING";
     }
 }
 
@@ -79,9 +93,10 @@
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     
-    // mmm vibrations, sound coming soon?
+    // vibrate to indicate recording has started
     [self vibrate];
-    [self performSelector:@selector(startRecording) withObject:nil afterDelay:0.6];
+    
+    // listen for sensor changes
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sensorStateChange:)
                                                  name:@"UIDeviceProximityStateDidChangeNotification" object:nil];
 }
@@ -89,87 +104,71 @@
 - (void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
     
-    // schedule a timer yo.
-    self.finishTimer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(stopRecording) userInfo:nil repeats:NO];
+    // schedule a timer to keep tracking of video length
+    self.videoIsLongEnough = NO;
+    self.videoRecordTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(startCameraCapture) userInfo:nil repeats:YES];
 }
 
 - (void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
     
+    // kill the timer, remove observer and reset the state of the VC
     [self.finishTimer invalidate];
+    [self.videoRecordTimer invalidate];
     [self.captureStatusLabel setText:@"RECORDING"];
     self.captureStatusLabel.textColor = [UIColor redColor];
-    [self stopVideoCapture];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 #pragma mark - Actions
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
-    NSLog(@"%@", info);
-    
-    if (!self.finishTimer.valid) {
-        [self vibrate];
 
-        NSURL *videoURL = [info objectForKey:UIImagePickerControllerMediaURL];
-        NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
-        
-        NSLog(@"%@ - %@", videoURL, mediaType);
-        
-        if (CFStringCompare((__bridge CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo)
-        {
-            NSString *moviePath = [[info objectForKey: UIImagePickerControllerMediaURL] path];
-            
-            
-            if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath))
-            {
-                UISaveVideoAtPathToSavedPhotosAlbum (moviePath, nil, nil, nil);
-            }
-        }
-        
-        // Convert to Video data.
-        
-        NSData *imageData = [NSData dataWithContentsOfURL:videoURL];        
-        PFFile *videofile = [PFFile fileWithName:@"video.mov" data:imageData];
-        
-        [videofile saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-            if (!error) {
-                [self dismissViewControllerAnimated:NO completion:nil];
-            }else{
-                NSLog(@"%@", error);
-            }
-        }];
-        
-    }else{
-        NSURL *videoURL = [info objectForKey:UIImagePickerControllerMediaURL];
-        
-        // delete the file.
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        if ([fileManager fileExistsAtPath:videoURL.path]) {
-            NSError *error;
-            if ([fileManager removeItemAtPath:[videoURL.path stringByDeletingLastPathComponent] error:&error] != YES) {
-                NSLog(@"Unable to delete file: %@", [error localizedDescription]);
-            }
-        }
-        
-        [self dismissViewControllerAnimated:NO completion:nil];
-
+    // video wasn't long enough. do nothing here but dismiss.
+    if (!self.videoIsLongEnough) {
+        return;
     }
+    
+    // save the video to disk
+    NSURL *videoURL = [info objectForKey:UIImagePickerControllerMediaURL];
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    
+    NSLog(@"%@ - %@", videoURL, mediaType);
+    
+    if (CFStringCompare((__bridge CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo)
+    {
+        NSString *moviePath = [[info objectForKey: UIImagePickerControllerMediaURL] path];
+        
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath))
+        {
+            UISaveVideoAtPathToSavedPhotosAlbum (moviePath, nil, nil, nil);
+        }
+    }
+    
+    [self dismissViewControllerAnimated:NO completion:^{
+        [self vibrate];
+    }];
+}
+
+- (void)startCameraCapture
+{
+    if ([self startVideoCapture]){
+        self.finishTimer = [NSTimer scheduledTimerWithTimeInterval:7 target:self selector:@selector(timerFinished) userInfo:nil repeats:NO];
+        [self.videoRecordTimer invalidate];
+    }
+}
+
+- (void)timerFinished{
+    [self stopVideoCapture];
+    self.videoIsLongEnough = YES;
 }
 
 - (void)vibrate{
     AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
 }
 
-- (void)startRecording{
-    [self startVideoCapture];
-    
-}
-
-- (void)stopRecording{
-    [self.finishTimer invalidate];
-    [self stopVideoCapture];
-    [self performSelector:@selector(vibrate) withObject:nil afterDelay:0.3];
+- (void)dismissVC{
+    [self dismissViewControllerAnimated:NO completion:nil];
 }
 
 @end
