4615ec38dfc71d2af3e5cc46a7c19d12297db24d
Eric Lewis <ericlewis777@gmail.com>
2015-11-24 21:10:01 -0800

    make following people crazy easier.

--- Beme/View Controllers/User/FindFriendsTableViewController.m
+++ Beme/View Controllers/User/FindFriendsTableViewController.m
@@ -8,8 +8,10 @@
 
 #import "FindFriendsTableViewController.h"
 
-@interface FindFriendsTableViewController ()
+#import <Parse/Parse.h>
 
+@interface FindFriendsTableViewController ()
+@property (nonatomic, strong) NSArray *allUsers;
 @end
 
 @implementation FindFriendsTableViewController
@@ -18,7 +20,76 @@
     [super commonInit];
     
     self.title = @"FIND FRIENDS";
+}
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
     
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self loadPeople];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class]) forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = user.username;
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    PFRelation *friendsRelation = [[PFUser currentUser] relationForKey:@"friendsRelation"];
+    
+    [friendsRelation addObject:user];
+
+    [[PFUser currentUser] saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@ %@", error, error.userInfo);
+        }
+    }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.allUsers.count;
+}
+
+#pragma mark - Helpers
+
+- (BOOL)isFriend:(PFUser *)user{
+    for (PFUser *friend in self.allUsers) {
+        if ([friend.objectId isEqualToString:user.objectId]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)loadPeople{
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query whereKey:@"objectId" notEqualTo:[[PFUser currentUser] objectId]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if(error){
+            NSLog(@"Error: %@ %@", error, error.userInfo);
+        } else {
+            self.allUsers = objects;
+            [self.tableView reloadData];
+        }
+    }];
 }
 
 @end
