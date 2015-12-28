5809eb75916b51ff4bd781de9a06c1fc098ae49b
Eric Lewis <ericlewis777@gmail.com>
2015-12-28 12:37:21 -0800

    playback supports skipping + has a progress bar.

--- Beme/View Controllers/Video/PlaybackViewController.m
+++ Beme/View Controllers/Video/PlaybackViewController.m
@@ -15,6 +15,8 @@
 @property (nonatomic, strong) MPMoviePlayerController *moviePlayer;
 @property (nonatomic, strong) NSArray *videos;
 @property (nonatomic, strong) PFObject *currentVideo;
+@property (nonatomic, strong) NSTimer *pollPlayerTimer;
+@property (nonatomic, strong) UIProgressView *progressBar;
 @property (nonatomic) NSInteger videoPlayCount;
 @end
 
@@ -25,6 +27,8 @@
         self.videoPlayCount = 0;
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackFinished) name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+        
+        // dismiss gesture.
         UISwipeGestureRecognizer *swipeDownGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(dismissVC)];
         swipeDownGesture.direction = UISwipeGestureRecognizerDirectionDown;
         [self.view addGestureRecognizer:swipeDownGesture];
@@ -43,11 +47,50 @@
         [self.moviePlayer setControlStyle:MPMovieControlStyleNone];
         [self.moviePlayer prepareToPlay];
         [self.moviePlayer setShouldAutoplay:YES];
-        
         [self.view addSubview:self.moviePlayer.view];
+        
+        UIButton *nextButton = [[UIButton alloc] initWithFrame:self.view.frame];
+        [nextButton setBackgroundColor:[UIColor clearColor]];
+        [nextButton setTitle:@"" forState:UIControlStateNormal];
+        [nextButton addTarget:self action:@selector(playbackFinished) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:nextButton];
+        
+        self.progressBar = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
+        [self.view addSubview:self.progressBar];
+        
+        [self.progressBar mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(self.mas_bottomLayoutGuideTop);
+            make.left.right.equalTo(self.view);
+        }];
+        
+        [self beginPlayerPolling];
     }
     
     return self;
+}
+
+- (void)beginPlayerPolling{
+    self.pollPlayerTimer = [NSTimer scheduledTimerWithTimeInterval:0.01
+                                                            target:self
+                                                          selector:@selector(pollTimerTick:)
+                                                          userInfo:nil
+                                                           repeats:YES];
+    
+}
+
+- (void)pollTimerTick:(NSObject *)sender {
+    if (self.moviePlayer.playbackState == MPMoviePlaybackStatePlaying && self.moviePlayer.currentPlaybackTime != 0.f){
+        [self.progressBar setProgress:self.moviePlayer.currentPlaybackTime / self.moviePlayer.duration];
+    }else{
+        [self.progressBar setProgress:0.0f];
+    }
+}
+
+- (void)endPlayerPolling {
+    if (self.pollPlayerTimer != nil){
+        [self.pollPlayerTimer invalidate];
+        self.pollPlayerTimer = nil;
+    }
 }
 
 - (void)playbackFinished{
@@ -55,7 +98,7 @@
     self.videoPlayCount++;
     
     // queue up the next one if we can
-    if (self.videoPlayCount < self.videos.count) {
+    if (self.videoPlayCount < self.videos.count){
         self.currentVideo = [self.videos objectAtIndex:self.videoPlayCount];
         PFFile *file = [self.currentVideo valueForKey:@"video"];
         NSURL *myURL = [NSURL URLWithString:file.url];
@@ -68,6 +111,7 @@
 }
 
 - (void)dismissVC{
+    [self endPlayerPolling];
     [self.moviePlayer stop];
     
     // clear out the watched video ID's.
