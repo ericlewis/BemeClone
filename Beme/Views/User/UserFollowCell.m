45506ae00218bdea706938d59e2b84d22cfc583b
Eric Lewis <ericlewis777@gmail.com>
2015-12-29 15:46:02 -0800

    fix following / unfollowing.

--- Beme/Views/User/UserFollowCell.m
+++ Beme/Views/User/UserFollowCell.m
@@ -34,38 +34,19 @@
 - (void)setUser:(PFUser *)user followers:(BOOL)isFollowers{
     self.user = user;
     
-    // cache data
-    NSDictionary *attributes = nil;
-    //[[Cache sharedCache] attributesForUser:self.user]
-    
-    // set follow status if we have a cache
-    if (attributes) {
-        [self setFollowing:[[Cache sharedCache] followStatusForUser:self.user]];
-
-    }else{
-        @synchronized(self) {
-            PFQuery *isFollowingQuery = [PFQuery queryWithClassName:kActivityClassKey];
-            [isFollowingQuery whereKey:kActivityTypeKey equalTo:kActivityTypeFollow];
-            
-            if (!isFollowers) {
-                [isFollowingQuery whereKey:kActivityFromUserKey equalTo:self.user];
-                [isFollowingQuery whereKey:kActivityToUserKey equalTo:[PFUser currentUser]];
-            }else{
-                [isFollowingQuery whereKey:kActivityToUserKey equalTo:self.user];
-                [isFollowingQuery whereKey:kActivityFromUserKey equalTo:[PFUser currentUser]];
-            }
-            
-            [isFollowingQuery setCachePolicy:kPFCachePolicyCacheThenNetwork];
-            
-            [isFollowingQuery countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
-                @synchronized(self) {
-                    [[Cache sharedCache] setFollowStatus:(!error && number > 0) user:self.user];
-                }
-                
-                [self setFollowing:(!error && number > 0)];
-            }];
-            
-        }
+    @synchronized(self) {
+        PFQuery *isFollowingQuery = [PFQuery queryWithClassName:kActivityClassKey];
+        [isFollowingQuery whereKey:kActivityTypeKey equalTo:kActivityTypeFollow];
+        
+        [isFollowingQuery whereKey:kActivityToUserKey equalTo:self.user];
+        [isFollowingQuery whereKey:kActivityFromUserKey equalTo:[PFUser currentUser]];
+        
+        [isFollowingQuery setCachePolicy:kPFCachePolicyCacheThenNetwork];
+        
+        [isFollowingQuery countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
+            [self setFollowing:(!error && number > 0)];
+        }];
+        
     }
     
     self.textLabel.text = self.user.username;
